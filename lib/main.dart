90e37ede277969f1ad986085471fa97bc8ab5cff
sananzahid005 <sananzahid05@gmail.com>
2025-03-02 16:02:44 +0500

    container pending

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
-            child: Column(
-          children: [
-            Text('my new project'),
-            Text('it is about personal information')
-          ],
-        ),
+          child: Column(
+            children: [
+              Text('my new project'),
+              Text('it is about personal information'),
+                Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.white,
+                child: Text('container 1'),
+              ),
+            ],
+          ),
         ),
       ),
     );
