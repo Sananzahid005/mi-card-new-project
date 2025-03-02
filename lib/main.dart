6f0e612f37b00f6c4f381ce0b2eed6a55b1c8657
sananzahid005 <sananzahid05@gmail.com>
2025-03-02 12:54:56 +0500

    mi_card_updated_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext){
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.tealAccent,
+        body: Container(
+          color: Colors.white,
+        ),
+
       ),
-    ),
-  );
+    );
+  }
 }
